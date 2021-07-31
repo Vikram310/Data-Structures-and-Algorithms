3e1b034961cfa978df9b6900404746d2cdcd8ae5
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2021-07-31 11:05:20 +0530

    Union and Intersection

---
+++ GFG/Union and Intersection of arrays/Intersection of arrays.py
@@ -0,0 +1,18 @@
+def printIntersection(arr1, arr2, m, n):
+    i, j = 0, 0
+    while i < m and j < n:
+        if arr1[i] < arr2[j]:
+            i += 1
+        elif arr2[j] < arr1[i]:
+            j+= 1
+        else:
+            print(arr2[j])
+            j += 1
+            i += 1
+ 
+# Driver program to test above function
+arr1 = [1, 2, 4, 5, 6]
+arr2 = [2, 3, 5, 7]
+m = len(arr1)
+n = len(arr2)
+printIntersection(arr1, arr2, m, n)
