bc66e35c20061d7cacb6a0ddc9376d9f7b5e2e3e
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-08-27 12:47:03 +0530

    Time: 29 ms (95.35%), Space: 13.8 MB (96.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        @cache
+        def climb(n):
+            if n ==1 :
+                return 1
+            if n == 2: 
+                return 2
+            return climb(n-1) + climb(n-2)
+        return climb(n)
