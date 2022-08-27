09ed72f5d91e3e15f08e407b9942a662c4cc00bd
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-08-27 12:35:57 +0530

    Time: 35 ms (83.46%), Space: 13.8 MB (96.45%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        
+        def dfs(n):
+            
+            a,b,c = 0,1,1
+            for i in range(n):
+                a,b,c = b,c,a+b+c
+            return a 
+        return dfs(n)
