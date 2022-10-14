cafc79947b6a855e14e7aaae79aefae93d23df69
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-10-14 10:39:33 +0530

    Time: 4541 ms (11.36%), Space: 60.6 MB (40.40%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.py
@@ -6,18 +6,25 @@
 class Solution:
     def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
+        # checking if it is a single node
         if not head.next:
             return None
         
+        #initialise two pointers tortoise and rabbit (slow,fast) to head(start) of linked list
         tortoise,rabbit = head,head
+        #initialise one more pointer prev, which is just before tortoise
         prev = None
         
+        # iterate till the last node
         while rabbit and rabbit.next:
-            prev = tortoise
-            tortoise = tortoise.next
-            rabbit = rabbit.next.next
-            
-        
+            prev = tortoise #prev moves one step ahead and points to tortoise node
+            tortoise = tortoise.next #tortoise takes one step fwd
+            rabbit = rabbit.next.next #rabbit takes two steps fwd
+        '''    
+        when rabbit is at last position, tortoise will be at exact middle poisition, and prev will 
+        before tortoise, as we need to remove the middle node, just point tortoise next to 
+        prev next
+        '''
         prev.next = tortoise.next
         
         return head
