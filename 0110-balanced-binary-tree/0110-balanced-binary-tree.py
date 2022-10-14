baa6f1504ad3c13c58e66702434953b8430a0f9b
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-10-14 11:36:56 +0530

    Time: 49 ms (97.39%), Space: 18.9 MB (11.13%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -7,12 +7,17 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         
+        #create another function for recursively using it on left and right subtree
         def dfs(root):
-            if not root:return [True,0]
+            #returns two pointers, one to show whether balanced and other is length
+            if not root:return [True,0] #if root is null, it is balanced and length = 0
             
+            #apply the same function on left and right childs
             left,right = dfs(root.left),dfs(root.right)
+            # tree is balanced only if left and right is balanced and also abs differences between 
+            # lengths of left and right shoudl be <= 1
             is_balanced = (left[0] and right[0]) and abs(left[1] - right[1]) <= 1
             
-            return [is_balanced,1+max(left[1],right[1])]
+            return [is_balanced,1+max(left[1],right[1])] #return whether tree is balanced and also length
         
         return dfs(root)[0]
