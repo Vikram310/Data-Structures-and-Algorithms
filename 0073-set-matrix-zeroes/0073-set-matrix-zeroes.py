47c540e9acf96eba8b92bce4e26dfc293295c8d5
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-10-17 14:44:15 +0530

    Time: 308 ms (29.44%), Space: 14.9 MB (16.47%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,34 +3,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = len(matrix)
-        cols = len(matrix[0])
+        m = len(matrix)
+        n = len(matrix[0])
         Firstrow = False
         
         #Find in which row n column do we have zero
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] == 0:
-                    matrix[0][c] = 0
-                    if r >0:
-                        matrix[r][0] = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    if i >0:
+                        matrix[i][0] = 0
                     else:
                         Firstrow = True
         
         #Fill rows and columns apart from first with zeroes wherever required
-        for r in range(1,rows):
-            for c in range(1,cols):
-                if matrix[0][c] == 0 or matrix[r][0] == 0:
-                    matrix[r][c] = 0
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
                     
         #Fill first column
         if matrix[0][0] == 0:
-            for r in range(rows):
-                matrix[r][0] = 0
+            for i in range(m):
+                matrix[i][0] = 0
         
         # Fill first row
         if Firstrow:
-            for c in range(cols):
-                matrix[0][c] = 0
+            for j in range(n):
+                matrix[0][j] = 0
         
         
