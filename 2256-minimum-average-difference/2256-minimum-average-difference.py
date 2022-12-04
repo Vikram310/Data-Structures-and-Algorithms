35f8b73322b7576c25c7e66435c9f29793abac85
Vikram Krishna <30468979+Vikram310@users.noreply.github.com>
2022-12-04 23:05:20 +0530

    Time: 2741 ms (32.56%), Space: 24.9 MB (56.92%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,29 +1,22 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         
-        res = [] #initialise result array
-        total = 0 # total sum of nums
+        sum_start, sum_end, res, diff, n = 0,0,0,1e9,len(nums)
         
-        for i in range(len(nums)):
-            total += nums[i]
-        
-        '''
-        sum_start = sum from starting to ith index
-        sum_end = sum from end to (i+1)th index
-        '''
-        n = len(nums)
-        sum_start, sum_end = 0, total
-        for i in range(len(nums)):
-            sum_start += nums[i]  
-            sum_end -= nums[i]
-            avg_start = (sum_start // float((i+1)))
-            if i == n-1:
-                avg_end = 0
-            else:
-                avg_end = (sum_end // float((n-i-1))) if i != n-1 else 0
-            #print(avg_start,avg_end)
-            avg = avg_start - avg_end if avg_start > avg_end else avg_end-avg_start
-            res.append(avg)
-
-        return res.index(min(res))
+        for i in range(n):
+            sum_end += nums[i]
             
+        for i in range(n):
+            sum_start += nums[i]
+            sum_end -= nums[i]
+            
+            avg1 = sum_start // (i+1)
+            avg2 = sum_end // (n-i-1) if i != n-1 else 0
+            
+            if abs(avg1 - avg2) < diff:
+                diff = abs(avg1-avg2)
+                res = i
+            
+        return res
+        
+        
